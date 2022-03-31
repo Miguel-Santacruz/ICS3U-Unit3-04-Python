a8f099039ee0e441f4f4c64ff221085a21dc48d6
Ubuntu <ubuntu@ip-172-31-43-72.ec2.internal>
2022-03-31 02:17:55 +0000

    positive, negatie or zero

---
+++ positive_negative_0.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Miguel Santacruz
+# Created on: March 2022
+# This program says the sign of an integer
+
+
+def main():
+    # This function says the sign of an integer
+
+    # Input
+    number = int(input("Enter an integer: "))
+
+    # Process & Output
+    print("")
+    if number > 0:
+        print("{} is positive!".format(number))
+    else:
+        if number < 0:
+            print("{} is negative!".format(number))
+        else:
+            print("{} is zero!".format(number))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
